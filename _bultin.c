618f277330576caeaa2fefdb36a2733f192ac7dd
zinne126 <ohaeriezinne7@gmail.com>
2023-06-27 06:09:33 -0700

    update

--- _bultin.c
+++ _bultin.c
@@ -93,5 +93,5 @@ int _myhelp(info_t *info)
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
 		_puts(*arg_array); /* temp att_unused workaround */
-	return (0)
-
+	return (0);
+}
